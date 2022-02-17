65ed9cfcf2904c8e21f949b6d3f31ae471904430
Alicia <arrowsrp@yahoo.com>
2022-02-17 11:28:34 -0800

    commit tree 6aed056a189015edb75eb26d79d2119ed6371c4d

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
-// Code EyesOnMe Component Here
+function EyesOnMe (){
+    function focusHandler(event){
+
+        if (event.type==='focus') {
+            console.log('Good!');
+
+        } else if (event.type==='blur'){
+            console.log('Hey! Eyes on me!');
+        }
+    }
+    return (
+    
+    <button onBlur={focusHandler} onFocus={focusHandler}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,9 @@
-// Code Keypad Component Here
+function Keypad (){
+    function changeHandle(event){
+        console.log('Entering password...')
+    }
+    return (
+    <input type="password" onChange={changeHandle} /> 
+    )
+}
+export default Keypad
